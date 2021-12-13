768943edd2f5bf22851afad6c4bb8100676e0eb8
crackerFactory64 <lee.mander@gmail.com>
2021-12-13 16:16:24 +0000

    complete

--- Day 13 - Dinner Calculator/index.js
+++ Day 13 - Dinner Calculator/index.js
@@ -1,5 +1,27 @@
-const btn = document.getElementById("btn")
-let food = document.getElementById("food")
+const btn = document.getElementById("btn");
+const food = document.getElementById("food");
+const guestsEl = document.getElementById("guests");
+const veggieEl = document.getElementById("vegetarian-input");
+const error = document.getElementById("error");
+
+function calculateDinner() {
+  const guests = guestsEl.value.trim();
+  const veggie = veggieEl.checked;
+
+  if (guests <= 0) {
+    error.classList.add("error--show");
+  } else {
+    error.classList.remove("error--show");
+
+    veggie == true
+      ? (food.innerHTML = "nut roast!")
+      : guests <= 4
+      ? (food.innerHTML = "turkey!")
+      : (food.innerHTML = "goose!");
+  }
+}
+
+btn.addEventListener("click", calculateDinner);
 
 // Tasks:
 // - Write the JS to decide the perfect Christmas dinner and render it in the result element. Don't forget to check whether the meal should be vegetarian!
@@ -7,9 +29,8 @@ let food = document.getElementById("food")
 // Dinner suggestions (or choose your own!):
 // Vegetarian: nut roast
 // 4 people or less: turkey
-// 5+ people: goose 
+// 5+ people: goose
 
 // Stretch goals:
 // - Add more dietary options.
 // - Show recipes when the meal has been selected.
-
